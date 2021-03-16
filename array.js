a69c39c88a7b721886409958bb42672ac120eca2
Thoriq Nur Faizal <thoriiiq@gmail.com>
2021-03-16 20:15:02 +0700

    adding materi array

---
+++ array.js
@@ -0,0 +1,23 @@
+let productTeam = [
+  "Product Manager",
+  "Frontend Developer",
+  "Backend Developer",
+  3,
+  true,
+];
+console.log(productTeam);
+console.log(productTeam[2]);
+productTeam[1] = "UI Designer";
+
+let arrayRandom = ["string", 0, true, "nama"];
+console.log(arrayRandom);
+
+// const in array
+
+const cars1 = ["tesla", "honda", "toyota"];
+cars1 = ["Ferrari"];
+console.log(cars1);
+
+const cars2 = ["tesla", "honda", "toyota"];
+cars2[3] = "Ferrari";
+console.log(cars2);
