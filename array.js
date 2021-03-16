b1690e24f9740026045417303219fcfbe65bfba6
Thoriq Nur Faizal <thoriiiq@gmail.com>
2021-03-16 21:26:22 +0700

    tambahan materi method array

--- array.js
+++ array.js
@@ -18,6 +18,74 @@ const cars1 = ["tesla", "honda", "toyota"];
 cars1 = ["Ferrari"];
 console.log(cars1);
 
+// method .push
+// cars2[3] = "Ferrari";
 const cars2 = ["tesla", "honda", "toyota"];
-cars2[3] = "Ferrari";
+cars2.push("Ferrari");
 console.log(cars2);
+
+// method .pop() digunakan untuk menghapus data paling terakhir
+cars2.pop();
+
+// method .shift digunakan untuk menghapus data paling awal
+cars2.shift();
+
+// method .unshift digunakan untuk menambahkan data paling awal
+cars2.unshift("Wuling");
+
+// method .sort digunakan untuk mengurutkan secara ascending atau descending
+let number = [1, 7, 15, 3, 24, 17];
+number.sort();
+
+// ascending yang benar
+let number = [1, 7, 15, 3, 24, 17];
+number.sort(function (a, b) {
+  return a - b;
+});
+
+// descending yang benar
+number.sort(function (a, b) {
+  return b - a;
+});
+
+let huruf = ["abc", "cicak", "delman", "becak"];
+huruf.sort();
+
+let huruf = ["abc", "cbc", "bbc", "dbc"];
+huruf.sort();
+
+// method .forEach
+const cars = ["tesla", "honda", "toyota"];
+// menggunakan function biasa
+cars.forEach(function (elements) {
+  console.log(elements);
+});
+// menggunakan arrow function
+cars.forEach((elements) => {
+  console.log(elements);
+});
+
+// method .map
+let number = [1, 2, 3, 4];
+number.map(function (elements) {
+  return elements * elements;
+});
+
+const cars = ["tesla", "honda", "toyota"];
+cars.map(function (elements) {
+  return elements + "s";
+});
+
+// method .map vs .foreach
+
+let number = [1, 2, 3, 4];
+number.map(function (elements) {
+  return elements * elements;
+});
+
+let number = [1, 2, 3, 4];
+number.forEach(function (elements, index) {
+  console.log((number[index] = elements * elements));
+});
+
+// soal 1
